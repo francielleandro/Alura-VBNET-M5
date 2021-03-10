7ea82e4648c7bb8bd4fde0a94c3dc7046cd0938a
Franciel Leandro <francielleandro@gmail.com>
2021-03-10 00:20:36 -0300

    Adicionando referencias de outro projeto e instanciando classes

--- ByteBank.SistemaAgencia/Frm_Secundario.Designer.vb
+++ ByteBank.SistemaAgencia/Frm_Secundario.Designer.vb
@@ -24,7 +24,15 @@ Partial Class Frm_Secundario
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_Secundario))
         Me.Pic_Logo = New System.Windows.Forms.PictureBox()
+        Me.Grp_ContaCorrente = New System.Windows.Forms.GroupBox()
+        Me.Txt_ContaCorrente = New System.Windows.Forms.TextBox()
+        Me.Txt_Agencia = New System.Windows.Forms.TextBox()
+        Me.Lbl_ContaCorrente = New System.Windows.Forms.Label()
+        Me.Lbl_Agencia = New System.Windows.Forms.Label()
+        Me.Btn_Criar = New System.Windows.Forms.Button()
+        Me.Lbl_ContaCorrenteCriada = New System.Windows.Forms.Label()
         CType(Me.Pic_Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Grp_ContaCorrente.SuspendLayout()
         Me.SuspendLayout()
         '
         'Pic_Logo
@@ -36,19 +44,95 @@ Partial Class Frm_Secundario
         Me.Pic_Logo.TabIndex = 0
         Me.Pic_Logo.TabStop = False
         '
+        'Grp_ContaCorrente
+        '
+        Me.Grp_ContaCorrente.Controls.Add(Me.Btn_Criar)
+        Me.Grp_ContaCorrente.Controls.Add(Me.Txt_ContaCorrente)
+        Me.Grp_ContaCorrente.Controls.Add(Me.Txt_Agencia)
+        Me.Grp_ContaCorrente.Controls.Add(Me.Lbl_ContaCorrente)
+        Me.Grp_ContaCorrente.Controls.Add(Me.Lbl_Agencia)
+        Me.Grp_ContaCorrente.Location = New System.Drawing.Point(159, 12)
+        Me.Grp_ContaCorrente.Name = "Grp_ContaCorrente"
+        Me.Grp_ContaCorrente.Size = New System.Drawing.Size(231, 150)
+        Me.Grp_ContaCorrente.TabIndex = 1
+        Me.Grp_ContaCorrente.TabStop = False
+        Me.Grp_ContaCorrente.Text = "GroupBox1"
+        '
+        'Txt_ContaCorrente
+        '
+        Me.Txt_ContaCorrente.Location = New System.Drawing.Point(10, 94)
+        Me.Txt_ContaCorrente.Name = "Txt_ContaCorrente"
+        Me.Txt_ContaCorrente.Size = New System.Drawing.Size(100, 20)
+        Me.Txt_ContaCorrente.TabIndex = 3
+        '
+        'Txt_Agencia
+        '
+        Me.Txt_Agencia.Location = New System.Drawing.Point(10, 37)
+        Me.Txt_Agencia.Name = "Txt_Agencia"
+        Me.Txt_Agencia.Size = New System.Drawing.Size(100, 20)
+        Me.Txt_Agencia.TabIndex = 2
+        '
+        'Lbl_ContaCorrente
+        '
+        Me.Lbl_ContaCorrente.AutoSize = True
+        Me.Lbl_ContaCorrente.Location = New System.Drawing.Point(7, 78)
+        Me.Lbl_ContaCorrente.Name = "Lbl_ContaCorrente"
+        Me.Lbl_ContaCorrente.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_ContaCorrente.TabIndex = 1
+        Me.Lbl_ContaCorrente.Text = "Label1"
+        '
+        'Lbl_Agencia
+        '
+        Me.Lbl_Agencia.AutoSize = True
+        Me.Lbl_Agencia.Location = New System.Drawing.Point(7, 20)
+        Me.Lbl_Agencia.Name = "Lbl_Agencia"
+        Me.Lbl_Agencia.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Agencia.TabIndex = 0
+        Me.Lbl_Agencia.Text = "Label1"
+        '
+        'Btn_Criar
+        '
+        Me.Btn_Criar.Location = New System.Drawing.Point(150, 110)
+        Me.Btn_Criar.Name = "Btn_Criar"
+        Me.Btn_Criar.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Criar.TabIndex = 4
+        Me.Btn_Criar.Text = "Button1"
+        Me.Btn_Criar.UseVisualStyleBackColor = True
+        '
+        'Lbl_ContaCorrenteCriada
+        '
+        Me.Lbl_ContaCorrenteCriada.AutoSize = True
+        Me.Lbl_ContaCorrenteCriada.Location = New System.Drawing.Point(159, 169)
+        Me.Lbl_ContaCorrenteCriada.Name = "Lbl_ContaCorrenteCriada"
+        Me.Lbl_ContaCorrenteCriada.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_ContaCorrenteCriada.TabIndex = 2
+        Me.Lbl_ContaCorrenteCriada.Text = "Label1"
+        '
         'Frm_Secundario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(654, 298)
+        Me.Controls.Add(Me.Lbl_ContaCorrenteCriada)
+        Me.Controls.Add(Me.Grp_ContaCorrente)
         Me.Controls.Add(Me.Pic_Logo)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Frm_Secundario"
         Me.Text = "Frm_Secundario"
         CType(Me.Pic_Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Grp_ContaCorrente.ResumeLayout(False)
+        Me.Grp_ContaCorrente.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Pic_Logo As PictureBox
+    Friend WithEvents Grp_ContaCorrente As GroupBox
+    Friend WithEvents Lbl_ContaCorrente As Label
+    Friend WithEvents Lbl_Agencia As Label
+    Friend WithEvents Txt_ContaCorrente As TextBox
+    Friend WithEvents Txt_Agencia As TextBox
+    Friend WithEvents Btn_Criar As Button
+    Friend WithEvents Lbl_ContaCorrenteCriada As Label
 End Class
--- ByteBank.SistemaAgencia/Frm_Secundario.vb
+++ ByteBank.SistemaAgencia/Frm_Secundario.vb
@@ -1,4 +1,6 @@
-﻿Public Class Frm_Secundario
+﻿Imports ByteBank.Bibliotecas.Classes.Clientes
+
+Public Class Frm_Secundario
     Public Sub New()
 
         ' This call is required by the designer.
@@ -8,5 +10,25 @@
 
         Me.Text = "Formulário Secundario"
 
+        Grp_ContaCorrente.Text = "Conta Corrente"
+
+        Lbl_Agencia.Text = "Agencia"
+
+        Lbl_ContaCorrente.Text = "Conta Corrente"
+
+        Lbl_ContaCorrenteCriada.Text = ""
+
+        Btn_Criar.Text = "Criar"
+
+    End Sub
+
+    Private Sub Btn_Criar_Click(sender As Object, e As EventArgs) Handles Btn_Criar.Click
+        Dim vAgencia As Integer = Val(Txt_Agencia.Text)
+        Dim vContaCorrente As Integer = Val(Txt_ContaCorrente.Text)
+
+        Dim Conta As New ContaCorrente(vAgencia, vContaCorrente)
+
+        Lbl_ContaCorrenteCriada.Text = "Agencia:" + vAgencia.ToString +
+            " - Conta: " + vContaCorrente.ToString
     End Sub
 End Class
