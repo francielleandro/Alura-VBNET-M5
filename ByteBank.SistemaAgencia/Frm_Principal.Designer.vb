f2181bde447dd15ec25e491b3b7841c7824af8ea
Franciel Leandro <francielleandro@gmail.com>
2021-03-09 23:59:38 -0300

    trabalhando com formularios MDI e adicionando formularios filhos utilizando o MdiParent

--- ByteBank.SistemaAgencia/Frm_Principal.Designer.vb
+++ ByteBank.SistemaAgencia/Frm_Principal.Designer.vb
@@ -23,20 +23,55 @@ Partial Class Frm_Principal
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_Principal))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Aula01ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Vídeo01ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Aula01ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Aula01ToolStripMenuItem
+        '
+        Me.Aula01ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Vídeo01ToolStripMenuItem})
+        Me.Aula01ToolStripMenuItem.Name = "Aula01ToolStripMenuItem"
+        Me.Aula01ToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
+        Me.Aula01ToolStripMenuItem.Text = "Aula 01"
+        '
+        'Vídeo01ToolStripMenuItem
+        '
+        Me.Vídeo01ToolStripMenuItem.Name = "Vídeo01ToolStripMenuItem"
+        Me.Vídeo01ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Vídeo01ToolStripMenuItem.Text = "Vídeo 01"
         '
         'Frm_Principal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Frm_Principal"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents Aula01ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Vídeo01ToolStripMenuItem As ToolStripMenuItem
 End Class
--- ByteBank.SistemaAgencia/Frm_Principal.vb
+++ ByteBank.SistemaAgencia/Frm_Principal.vb
@@ -8,4 +8,10 @@
         Me.Text = "Sistema de Agências ByteBank"
 
     End Sub
+
+    Private Sub Vídeo01ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Vídeo01ToolStripMenuItem.Click
+        Dim F As New Frm_Secundario
+        F.MdiParent = Me
+        F.Show()
+    End Sub
 End Class
